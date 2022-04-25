a8c3628a3cf89a04c00c8215df9ae94891e6bd82
Manuel Perez <92071461+manuelp0635@users.noreply.github.com>
2022-04-25 17:04:41 -0500

    Add files via upload

---
+++ Sitio web/js/ejemplo.js
@@ -0,0 +1,12 @@
+function findLargest(numbers){
+    // Your code goes here
+    console.log(findLargest(19));
+}
+
+function computeClosestToZero(ts){
+    // Write your code here
+    // To debug: console.error('Debug messaages...');
+    // 0 ≤ ts size ≤ 10000
+    // 100 ≥ ts size ≥ 10000
+    return -1;
+}
